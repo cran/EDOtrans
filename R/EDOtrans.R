05ae92c9d2b68367197d29d144b3accba348eea2
Jorn Lotsch <j.lotsch@em.uni-frankfurt.de>
2021-11-10 13:10:02 +0000

    version 0.2.0

--- R/EDOtrans.R
+++ R/EDOtrans.R
@@ -1,68 +1,58 @@
-#Performs EDO transformation on given data and classes
+# Performs EDO transformation on given data and classes
 #' @importFrom ABCanalysis ABCanalysis
-#' @importFrom AdaptGauss BayesDecisionBoundaries
 #' @importFrom methods hasArg
-#' @importFrom stats dnorm median na.omit sd
-#' @importFrom DistributionOptimization DistributionOptimization
+#' @importFrom stats median sd
+#' @importFrom opGMMassessment opGMMassessment
 #' @export
-EDOtrans <- function(Data, Cls, Means, SDs, Weights, DO = FALSE, PlotGMM = FALSE) {
-  if (hasArg("Data") == FALSE)
+EDOtrans <- function(Data, Cls, PlotIt = FALSE, FitAlg = "normalmixEM", Criterion = "LR",
+  MaxModes = 8, MaxCores = 2048, Seed) {
+  if (hasArg("Data") == FALSE) {
     stop("EDOtrans: No data provided. Stopping.")
-  is.integer0 <- function(x)
-  {
+  }
+
+  is.integer0 <- function(x) {
     is.integer(x) && length(x) == 0L
   }
 
-  #Obtain standard deviations
-  if (hasArg("Means") == TRUE & hasArg("SDs") == TRUE & hasArg("Weights") == TRUE) {
-    if (length(c(Means, SDs, Weights)) %% 3 != 0)
-      stop("EDOtrans: Unequal number of modes in parameters.")
-    Means0 <- Means
-    SDs0 <- SDs
-    Weights0 <- Weights
-    Boundaries <- c()
-    Cls <- rep(1, length(Data))
-    if (length(Means0) > 1) {
-      Boundaries <- AdaptGauss::BayesDecisionBoundaries(Means = Means0, SDs = SDs0, Weights = Weights0)
-      if (is.integer0(Boundaries) == FALSE)
-        Boundaries <- Boundaries[Boundaries >= min(Means0) & Boundaries <= max(Means0)]
-      if (length(Boundaries) > 0) {
-        Cls <- cutGMM(x = Data, breaks = Boundaries)
-      }
-    }
-
+  if (!missing(Seed)) {
+    ActualSeed <- Seed
   } else {
-    if (hasArg("Cls") == TRUE) {
-      if (length(Cls) != length(Data)) {
-        stop("EDOtrans: Unequal legths of Data and Cls.")
-      } else {
-        Means0 <- tapply(X = Data, INDEX =  Cls, FUN = mean)
-        SDs0 <- tapply(X = Data, INDEX =  Cls, FUN = sd)
-        Weights0 <- tapply(X = Data, INDEX =  Cls, function(x) length(x) / length(Data))
-      }
-    } else {
-      #Obtain classes via GMM
-      warning("EDOtrans: Classes created using Gaussian mixtures.", call. = FALSE)
-      if (hasArg("DO") == TRUE) DO = DO else DO = FALSE
-      if (hasArg("PlotGMM") == TRUE) PlotGMM = PlotGMM else PlotGMM = FALSE
-      GMMresults  <- GMMasessment(Data = Data, DO = DO, PlotIt = PlotGMM)
-      Cls <- GMMresults$Cls
-      Means0 <- GMMresults$Means
-      SDs0 <- GMMresults$SDs
-      Weights0 <- GMMresults$Weights
-    }
+    ActualSeed <- tail(get(".Random.seed", envir = globalenv()), 1)
   }
 
-  #Selection of dominant groups
+  # Main part If classes are specified, transformation is done based on the
+  # classes, otherwise the modality is checked automatically.
+  if (hasArg("Cls") == TRUE) {
+    if (length(Cls) != length(Data)) {
+      stop("EDOtrans: Classes provided but unequal legths of Data and Cls.")
+    } else {
+      Means0 <- tapply(X = Data, INDEX = Cls, FUN = mean)
+      SDs0 <- tapply(X = Data, INDEX = Cls, FUN = sd)
+      Weights0 <- tapply(X = Data, INDEX = Cls, function(x) length(x)/length(Data))
+    }
+  } else {
+    # Obtain classes via opGMMassessment
+    warning("EDOtrans: Classes created using Gaussian mixture modeling.", call. = FALSE)
+    GMMresults <- opGMMassessment(Data = Data, FitAlg = FitAlg, Criterion = Criterion,
+      MaxModes = MaxModes, MaxCores = MaxCores, PlotIt = PlotIt, KS = FALSE,
+      Seed = ActualSeed)
+    Cls <- GMMresults$Cls
+    Means0 <- GMMresults$Means
+    SDs0 <- GMMresults$SDs
+    Weights0 <- GMMresults$Weights
+  }
+
+  # Selection of dominant groups
   if (length(Weights0) > 1) {
     WeightsABC <- ABCanalysis::ABCanalysis(as.vector(Weights0))
     if (is.integer0(WeightsABC$Aind) == FALSE) {
       Means0 <- Means0[WeightsABC$Aind]
       SDs0 <- SDs0[WeightsABC$Aind]
       Weights0 <- Weights0[WeightsABC$Aind]
-      #Combine standard deviations  from dominant groups
+      # Combine standard deviations from dominant groups
       nDom <- sum(Weights0) * length(Data)
-      CombinedDominatGroupsParameters <- combinedModesParameters(Means = Means0, SDs = SDs0, Weights = Weights0, n = nDom)
+      CombinedDominatGroupsParameters <- combinedModesParameters(Means = Means0,
+        SDs = SDs0, Weights = Weights0, n = nDom)
       SDdomSq <- CombinedDominatGroupsParameters$SD * sqrt(2)
     } else {
       SDdomSq <- median(SDs0) * sqrt(2)
@@ -71,8 +61,8 @@ EDOtrans <- function(Data, Cls, Means, SDs, Weights, DO = FALSE, PlotGMM = FALSE
     SDdomSq <- SDs0 * sqrt(2)
   }
 
-  #Perform EDO transformation
-  DataEDOtrans <- Data / SDdomSq
+  # Perform EDO transformation
+  DataEDOtrans <- Data/SDdomSq
 
   return(list(DataEDO = DataEDOtrans, EDOfactor = SDdomSq, Cls = Cls))
 }
--- R/combinedModesParameters.R
+++ R/combinedModesParameters.R
@@ -1,5 +1,4 @@
-#Calculates parameters of combined Gaussian modes
-
+# Calculates parameters of combined Gaussian modes
 combinedModesParameters <- function(Means, SDs, Weights, n) {
   Means0 <- Means[order(Means)]
   SDs0 <- SDs[order(Means)]
@@ -8,9 +7,9 @@ combinedModesParameters <- function(Means, SDs, Weights, n) {
   if (sumWeights != 1) {
     Weights0 <- Weights0 / sumWeights
   }
-  Ns <- Weights0 * n
+Ns <- Weights0 * n
   MeanAll <- sum(Means0 * Weights0)
-  qc <- sum((Ns - 1) * SDs0 ^ 2 + Ns * Means0 ^ 2)
-  sc <- sqrt((qc - (n) * MeanAll ^ 2) / (n - 1))
+  qc <- sum((Ns - 1) * SDs0^2 + Ns * Means0^2)
+  sc <- sqrt((qc - (n) * MeanAll^2) / (n - 1))
   return(list(Mean = MeanAll, SD = sc))
 }
